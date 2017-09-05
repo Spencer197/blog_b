31120ae03548e75324eb60d3a4da369da8aebb64
Spencer197 <spncr@protonmail.com>
2017-09-05 14:49:17 +0000

    Add nested route for comments 6.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+ root 'welcome#index'
   
-  resources :articles
-  
-  root 'welcome#index'
+  resources :articles do
+    resources :comments
+  end
 end
+=begin
+This creates comments as a nested resource within articles. This is another part of capturing the hierarchical relationship that exists 
+between articles and comments.
+=end
