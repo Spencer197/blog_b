c01647756b88de7b6159de3ce564007769700a63
Spencer197 <spncr@protonmail.com>
2017-09-06 12:53:46 +0000

    Add destroy action 8

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,15 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)#Once we have made the new comment, we send the user back to the original article using the article_path(@article) helper.
   end
  
+  #The destroy action will find the article we are looking at, locate the comment within the @article.comments collection, and then remove it 
+  #from the database and send us back to the show action for the article:
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+  
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
