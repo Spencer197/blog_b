096096abb19e275f03ccd2865ff793ed90220501
Spencer197 <spncr@protonmail.com>
2017-09-06 11:49:32 +0000

    Refactor 7

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    redirect_to article_path(@article)#Once we have made the new comment, we send the user back to the original article using the article_path(@article) helper.
   end
  
   private
