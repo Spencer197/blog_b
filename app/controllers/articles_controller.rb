95779c25b69b25598b575eafc3ae12d80434dfc0
Spencer197 <spncr@protonmail.com>
2017-09-06 13:15:19 +0000

    Add authentication 9

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  
+    http_basic_authenticate_with name: "foo", password: "bar", except: [:index, :show]    
+    
     def index
         @articles = Article.all
     end
